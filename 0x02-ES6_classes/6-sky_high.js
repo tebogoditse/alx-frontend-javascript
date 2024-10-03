4ec3cfd265312693f8a2e59e83efa0ec449b9e12
tebogoditse <tebogoditse2@gmail.com>
2024-10-04 01:13:23 +0200

    6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  set floors(floors) {
+    if (typeof floors === 'number') {
+      this._floors = floors;
+    } else {
+      throw new TypeError('Floors must be a number');
+    }
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
