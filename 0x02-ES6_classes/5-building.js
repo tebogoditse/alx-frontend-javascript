02542658a94704ad84d956d16cd0595d96d0f090
tebogoditse <tebogoditse2@gmail.com>
2024-10-04 00:57:59 +0200

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new TypeError('Sqrt must me a number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
